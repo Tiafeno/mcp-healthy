921bda1847f00413b7e2469502c4a4a4fd360be8
Tiafeno <tiafenofnel@gmail.com>
2025-11-06 11:25:19 +0300

    Feat: Add `dict` serialization methods and update datetime types for models

--- models/conversation.py
+++ models/conversation.py
@@ -5,19 +5,27 @@ import uuid
 
 class Conversation(SQLModel, table=True):
     __tablename__ = "ai_conversations"
-    
+
     uuid: str | None = Field(default=None, primary_key=True)
     title: str
     user_id: str
     last_message: str|None = None
     last_message_role: str|None = None
-    created_at: str
-    updated_at: str | None = None
-    deleted_at: str | None = None
-    
+    created_at: datetime = Field()
+    updated_at: datetime | None = None
+    deleted_at: datetime | None = None
+
+    def dict(self):
+        return {
+            "uuid": self.uuid.__str__(),
+            "title": self.title,
+            "last_message": self.last_message,
+            "created_at": self.created_at.isoformat(),
+        }
+
 class Message(SQLModel, table=True):
     __tablename__ = "ai_conversation_messages"
-    
+
     uuid: str | None = Field(default=None, primary_key=True)
     conversation_id: str
     external_id : str | None = None
@@ -25,7 +33,7 @@ class Message(SQLModel, table=True):
     content: str
     created_at: datetime = Field()
     updated_at: datetime | None = None
-    
+
     def dict(self):
         return {
             "uuid": self.uuid.__str__(),
